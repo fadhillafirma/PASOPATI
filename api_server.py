9356ab35075909810ee494bf30ee0111b3dcf350
fadhillafirmaa <firmafadhi@gmail.com>
2026-05-31 23:17:26 +0700

    lalu

--- api_server.py
+++ api_server.py
@@ -320,4 +320,5 @@ async def get_boundary():
 if __name__ == "__main__":
     import uvicorn
 
-    uvicorn.run(app, host="127.0.0.1", port=8000)
+    port = int(os.environ.get("PORT", 8080))
+    uvicorn.run(app, host="0.0.0.0", port=port)
